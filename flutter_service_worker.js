a717c5412e41f42454c0e0e2994746c2ae4f9298
doribari <152272623+doribari@users.noreply.github.com>
2024-04-05 09:36:09 +0900

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,8 +7,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "0db35ae7a415370b89e807027510caf0",
-"assets/NOTICES": "0a8d05c8b1869580511ed2082285c3f6",
+"assets/fonts/MaterialIcons-Regular.otf": "6590ec40485e17a27f9d92ff39aeaee8",
+"assets/NOTICES": "30c3b80001005cfd966309a7bafe37a0",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
@@ -27,11 +27,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "72162d7a36c60895a4b22b893d57f43d",
-"/": "72162d7a36c60895a4b22b893d57f43d",
-"main.dart.js": "70dc1c51f89dd678ce97b2ba116458b9",
-"manifest.json": "738f60da99b37519311af5aedd2943c3",
-"version.json": "08c57150d1142d1b1c6c08fd28289daa"};
+"index.html": "0e96aadcba03348ab338ba28a317b8c0",
+"/": "0e96aadcba03348ab338ba28a317b8c0",
+"main.dart.js": "f092db0d5a7e68fc508e6f02718f3d95",
+"manifest.json": "3b1793c5f10e32e56fcaf584d815d83c",
+"version.json": "9ea15f182da48af47fc1f507af825f48"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
